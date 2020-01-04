4cd59645cb7696c0092bd86c0d6c9386219ea6d3
reza_hosseini <srh.umir@gmail.com>
2020-01-04 15:18:39 +0100

    config file template for notebooks

---
+++ config_tag_recommender_template.py
@@ -0,0 +1,7 @@
+str_aggregated_path = r'..\..\data\MA14KD_AGGREGATED.csv'
+str_genome_scores = r'..\..\data\ml-20m\genome-scores.csv'
+str_tag_ids = r'..\..\data\ml-20m\genome-tags.csv'
+str_rating_path = r'..\..\data\ml-20m\ratings.csv'
+str_data_folder = r'..\..\data'
+minimum_no_of_frames = 10
+number_of_tag_per_movie = 10
